a80b816374bdf991aebb8d0b81d0069df64244cc
Balian of Ibelin <balian1belin@yandex.ru>
2024-10-21 09:49:13 +0300

    Use new version color in update text

--- pacstall-check-update.py
+++ pacstall-check-update.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import argparse, urllib.request, json, subprocess, os
-from typing import List, Any
+import argparse, urllib.request, json, subprocess, os, re
+from typing import List, Tuple, Any
 
 def run_parser() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
@@ -33,15 +33,24 @@ def run_parser() -> argparse.Namespace:
 
 def check_version(update: bool, non_zero: bool, notify: bool, notify_icon: str) -> int:
     new_version: str = ''
+    new_color: str = '\033[1m'
     with urllib.request.urlopen('https://api.github.com/repos/pacstall/pacstall/releases?page=1&per_page=1') as url:
         data: List[Any] = json.load(url)
         new_version = data[0]['tag_name']
+        match: re.Match[str] | None = re.search(
+            'https://www.htmlcsscolor.com/preview/128x128/(.+).png',
+            data[0]['body']
+        )
+        if match:
+            hex: str = match.group(1)
+            rgb: Tuple[str, ...] = tuple(str(int(hex[i:i+2], 16)) for i in (0, 2, 4))
+            new_color = f'\033[1m\x1b[38;2;{rgb[0]};{rgb[1]};{rgb[2]}m'
 
     pacstall_v: bytes = subprocess.check_output(['pacstall', '-V'])
     current_version: str = pacstall_v.decode('utf-8').split()[0]
 
     if current_version != new_version:
-        print(f'\033[93mPacstall\033[0m can be updated to version \033[1m{new_version}\033[0m')
+        print(f'\033[1mPacstall\033[0m can be updated to version {new_color}{new_version}\033[0m')
         if notify:
             subprocess.run(['notify-send', '-i', notify_icon, 'Pacstall update available', f'New version: {new_version}'])
         if update:
